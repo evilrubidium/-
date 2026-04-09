2acc847d1b592f400dfcc3f5bb4cfc121c1c1fb8
evilrubidium <75392445+evilrubidium@users.noreply.github.com>
2026-04-09 23:12:30 +0300

    Update variables.tf

--- terraform-03 /variables.tf
+++ terraform-03 /variables.tf
@@ -111,3 +111,95 @@ variable "each_vm" {
     { vm_name = "replica", cpu = 2, ram = 2, disk_volume = 10 }
   ]
 }
+
+# Параметры VM 
+
+variable "platform_id" {
+  description = "Платформа VM"
+  type        = string
+  default     = "standard-v1"
+}
+
+variable "image_family" {
+  description = "Семейство образа ОС"
+  type        = string
+  default     = "ubuntu-2204-lts"
+}
+
+# Web VM (count)
+
+variable "web_vm_count" {
+  description = "Количество web VM"
+  type        = number
+  default     = 2
+}
+
+variable "web_vm_resources" {
+  description = "Ресурсы web VM"
+  type = object({
+    cores  = number
+    memory = number
+  })
+  default = {
+    cores  = 2
+    memory = 2
+  }
+}
+
+variable "web_vm_disk_size" {
+  description = "Размер диска web VM"
+  type        = number
+  default     = 10
+}
+
+# Storage VM 
+
+variable "storage_vm_name" {
+  description = "Имя storage VM"
+  type        = string
+  default     = "storage"
+}
+
+variable "storage_vm_resources" {
+  description = "Ресурсы storage VM"
+  type = object({
+    cores  = number
+    memory = number
+  })
+  default = {
+    cores  = 2
+    memory = 2
+  }
+}
+
+variable "storage_boot_disk_size" {
+  description = "Размер загрузочного диска storage VM"
+  type        = number
+  default     = 10
+}
+
+# Доп диски
+
+variable "disk_count" {
+  description = "Количество дополнительных дисков"
+  type        = number
+  default     = 3
+}
+
+variable "disk_size" {
+  description = "Размер дополнительного диска (ГБ)"
+  type        = number
+  default     = 1
+}
+
+variable "disk_type" {
+  description = "Тип диска"
+  type        = string
+  default     = "network-ssd"
+}
+
+variable "disk_prefix" {
+  description = "Префикс имени дисков"
+  type        = string
+  default     = "extra-disk"
+}
