7d8b5c1330a703ceede9bf1b608a86e73680287c
evilrubidium <75392445+evilrubidium@users.noreply.github.com>
2026-04-09 23:08:24 +0300

    Update disk_vm.tf

--- terraform-03 /disk_vm.tf
+++ terraform-03 /disk_vm.tf
@@ -1,34 +1,34 @@
-# Подключил образ Ubuntu через data source
+# Data source
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2204-lts"
+  family = var.image_family
 }
 
-# Создал 3 одинаковых диска размером 1 Гб
+# Диски
 resource "yandex_compute_disk" "extra_disks" {
-  count     = 3
+  count     = var.disk_count
   name      = "extra-disk-${count.index + 1}"
-  size      = 1
-  type      = "network-ssd"
+  size      = var.disk_size
+  type      = var.disk_type
   zone      = var.zone
   folder_id = var.folder_id
 }
 
-# Создал VM "storage" и подключаем к ней дополнительные диски
+# VM storage
 resource "yandex_compute_instance" "storage" {
-  name        = "storage"
-  platform_id = "standard-v1"
+  name        = var.storage_vm_name
+  platform_id = var.platform_id
   folder_id   = var.folder_id
   zone        = var.zone
 
   resources {
-    cores  = 2
-    memory = 2
+    cores  = var.storage_vm_resources.cores
+    memory = var.storage_vm_resources.memory
   }
 
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.id
-      size     = 10
+      size     = var.storage_boot_disk_size
     }
   }
 
@@ -37,7 +37,6 @@ resource "yandex_compute_instance" "storage" {
     nat       = true
   }
 
-  # Динамически подключил созданные диски
   dynamic "secondary_disk" {
     for_each = yandex_compute_disk.extra_disks
     content {
